750804346a6b744a5374c7588d1037f9847284e2
collisionbox <21080012mk@edu.fca.ac.jp>
2022-10-11 10:06:50 +0900

    20221011

---
+++ TeamProOnibi/Assets/OnimenBehavior.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class OnimenBehavior : MonoBehaviour
+{
+    public GameObject player;
+    public float T = 1.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //transform.LookAt(player.transform);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.LookAt(player.transform);
+       
+        float f = 1.0f / T;
+        float y = Mathf.Sin(2 * Mathf.PI * f * Time.time);
+        this.transform.position = new Vector3(0,y,0);//èoåªà íuÇíÜêSÇ…ï‚ê≥Ç∑ÇÈÇ◊Çµ
+    }
+}
